8a5f3e649b7b78359a3b98fd61e405d2ad5dc3ba
Goitseone_Themba <97857682+Goitseone-Themba@users.noreply.github.com>
2024-06-19 16:43:56 +0200

    add listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class Property {
+  final String title;
+  final String details;
+  final int price;
+
+  Property({required this.title, required this.details, required this.price});
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+List<Property> properties = [
+  Property(title: "3 bedrooms Gwest", details: "A 3 bedroomed house in Gwest", price: 500000),
+  Property(title: "4 bedrooms Gwest", details: "A 4 bedroomed house in Gwest", price: 800000),
+  Property(title: "3 bedrooms Ext", details: "A 3 bedroomed house in Ext", price: 600000),
+];
 
   @override
   Widget build(BuildContext context) {
@@ -13,30 +27,53 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: const Text("Type shit"),
+          title: const Text("Hi, Jonathan"),
         ),
         body: Stack(
           children: [
             Container(
               color: Colors.white24,
             ),
-            const Center(
+            Center(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
+                  const Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [Text("this row")],
                   ),
-                  Row(
+                  const Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [Text("That row")],
                   ),
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: [
+                  //           Text("This other row"),
+                  //   ],
+                  // ),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                            Text("This other row"),
+                    children: <Widget>[
+                      Expanded(
+                        child: SizedBox(
+                          height: 300.0,
+                          child: ListView.builder(
+                            itemCount: properties.length,
+                            itemBuilder: (BuildContext pcontext, int index) {
+                              return Card(
+                                child: const ListTile(
+                                  leading: FlutterLogo(
+                                    size: 25.0,
+                                  ),
+                                  title: Text(properties[index].title),
+                                  subtitle: Text(properties[index].details),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ],
